fab800a2cb6116c343609352dc1ae29a120937b2
Kyrian Obikwelu <koshnawaza@gmail.com>
2026-05-23 01:17:53 +0100

    feat: add Windows DLL search path setup for libvips dependencies

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -24,6 +24,29 @@ $platformDirectory = Platform::findBestMatch($platformDirectories);
 if (false !== $platformDirectory) {
     $libDir = __DIR__.'/../lib/'.$platformDirectory;
 
+    if (Platform::isWindows()) {
+        $msvcrt = \FFI::cdef(
+            'size_t mbstowcs(void *wcstr, const char *mbstr, size_t count);',
+            'msvcrt.dll'
+        );
+
+        $k32 = \FFI::cdef(
+            '
+            int SetDefaultDllDirectories(unsigned long DirectoryFlags);
+            void* AddDllDirectory(const void* NewDirectory);
+            int RemoveDllDirectory(void* Cookie);',
+            'kernel32.dll'
+        );
+
+        $k32->SetDefaultDllDirectories(0x1000); // LOAD_LIBRARY_SEARCH_DEFAULT_DIRS
+
+        $pathLen = \strlen($libDir);
+        $wide = $msvcrt->new('char['.($pathLen * 2 + 2).']');
+        $msvcrt->mbstowcs($wide, $libDir, $pathLen + 1);
+
+        $k32->AddDllDirectory($wide);
+    }
+
     if (file_exists($libDir)) {
         VipsFFI::addLibraryPath($libDir);
     }
